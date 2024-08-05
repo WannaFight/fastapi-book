8b22fb766e2c8b7be1f92a1380582144d6e62b71
Ivan Trushin <itrushin99@gmail.com>
2024-08-05 06:32:13 +0300

    feat: add services

---
+++ src/service/creature.py
@@ -0,0 +1,33 @@
+import src.fake.creature as data
+from src.model.creature import Creature
+
+
+def get_all() -> list[Creature]:
+    """Return all creatures."""
+
+    return data.get_all()
+
+
+def get_one(name: str) -> Creature | None:
+    """Return creature with specified name."""
+    return data.get_one(name)
+
+
+def create(creature: Creature) -> Creature:
+    """Add an creature."""
+    return data.create(creature)
+
+
+def modify(creature: Creature) -> Creature:
+    """Partially modify an creature."""
+    return data.modify(creature)
+
+
+def replace(creature: Creature) -> Creature:
+    """Completely replace an creature."""
+    return data.replace(creature)
+
+
+def delete(name: str) -> None:
+    """Delete an creature; return None if it existsed."""
+    return data.delete(name)
---
+++ src/service/explorer.py
@@ -0,0 +1,33 @@
+import src.fake.explorer as data
+from src.model.explorer import Explorer
+
+
+def get_all() -> list[Explorer]:
+    """Return all creatures."""
+
+    return data.get_all()
+
+
+def get_one(name: str) -> Explorer | None:
+    """Return explorer with specified name."""
+    return data.get_one(name)
+
+
+def create(explorer: Explorer) -> Explorer:
+    """Add an explorer."""
+    return data.create(explorer)
+
+
+def modify(explorer: Explorer) -> Explorer:
+    """Partially modify an explorer."""
+    return data.modify(explorer)
+
+
+def replace(explorer: Explorer) -> Explorer:
+    """Completely replace an explorer."""
+    return data.replace(explorer)
+
+
+def delete(name: str) -> None:
+    """Delete an explorer; return None if it existsed."""
+    return data.delete(name)
